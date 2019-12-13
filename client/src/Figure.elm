7b4d24fbbc09fb21e4751504bf19cefbd3dc77a4
Daan van Berkel <daan.van.berkel@alliander.com>
2019-12-13 21:15:30 +0100

    Create a Browser.element

--- client/src/Figure.elm
+++ client/src/Figure.elm
@@ -1,25 +1,52 @@
 module Figure exposing (..)
 
+-- elm install avh4/elm-color
+
+import Browser
 import Canvas exposing (..)
 import Canvas.Settings exposing (..)
-import Color -- elm install avh4/elm-color
+import Color
 import Html exposing (Html)
 import Html.Attributes exposing (style)
 
-view : Html msg
-view =
+
+main : Program () () msg
+main =
+    Browser.element
+        { init = \_ -> ( init, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+init =
+    ()
+
+view : model -> Html msg
+view _ =
     let
-        width = 500
-        height = 300
+        width =
+            500
+
+        height =
+            300
     in
-        Canvas.toHtml (width, height)
-            [ style "border" "1px solid black" ]
-            [ shapes [ fill Color.white ] [ rect (0, 0) width height ]
-            , renderSquare
-            ]
+    Canvas.toHtml ( width, height )
+        [ style "border" "1px solid black" ]
+        [ shapes [ fill Color.white ] [ rect ( 0, 0 ) width height ]
+        , renderSquare
+        ]
+
 
 renderSquare =
-  shapes [ fill (Color.rgba 0 0 0 1) ]
-      [ rect (0, 0) 100 50 ]
+    shapes [ fill (Color.rgba 0 0 0 1) ]
+        [ rect ( 0, 0 ) 100 50 ]
 
-main = view
+
+update : msg -> model -> ( model, Cmd msg )
+update _ m =
+    (m, Cmd.none)
+
+subscriptions : model -> Sub msg
+subscriptions model =
+    Sub.none
